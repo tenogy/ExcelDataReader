f8069df4c3ea63969ad094ad572772414bad55b5
Yuriy Korpachan <korpachan@gmail.com>
2016-11-29 01:15:03 +0300

    dotnet core projects

---
+++ CoreTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var stream = new FileStream(@"..\Excel.Tests.4.5\Resources\Test_BigFormatted.xlsx", FileMode.Open))
+			{
+				IExcelDataReader reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+				reader.IsFirstRowAsColumnNames = firstRowNamesCheckBox.Checked;
+				var ds = reader.AsDataSet();
+			}
+		}
+	}
+}
